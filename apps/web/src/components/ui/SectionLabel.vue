d8a4b78a3b381dcf1628d2dba04afcd539dffcdb
Andrew Molyuk <andrew@molyuk.com>
2026-09-22 19:50:15 +0300

    chore: reset project

--- apps/web/src/components/ui/SectionLabel.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-defineProps<{ text: string }>()
-</script>
-
-<template>
-  <p class="inline-block font-ui text-[11px] font-semibold tracking-[0.12em] uppercase text-brand">
-    {{ text }}
-  </p>
-</template>
